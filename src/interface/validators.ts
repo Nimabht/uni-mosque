1c8237d5e426712ba0cc9cdf328b27af8fc76788
Nima <nima@gardoon.io>
2024-05-04 14:29:46 +0330

    Add interface for signup and login information

---
+++ src/interface/validators.ts
@@ -0,0 +1,13 @@
+export interface SignupInfo {
+  first_name: string;
+  last_name: string;
+  username: string;
+  password: string;
+  repeat_password: string;
+  phone_number: string;
+  role?: string;
+}
+export interface LoginInfo {
+  username: string | undefined;
+  password: string | undefined;
+}
